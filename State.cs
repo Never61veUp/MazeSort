d26c7d0969a84b3166379dca0132e43fca8e2488
Aleksey <134203994+Never61veUp@users.noreply.github.com>
2025-10-26 17:56:45 +0500

    Initial commit

---
+++ State.cs
@@ -0,0 +1,23 @@
+﻿namespace MazeSort;
+
+public record State(string Hallway, string[] Rooms)
+{
+    public virtual bool Equals(State? obj)
+    {
+        if (obj is not State other)
+            return false;
+
+        return Hallway == other.Hallway &&
+               Rooms.SequenceEqual(other.Rooms);
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = Hallway.GetHashCode();
+        
+        foreach (var room in Rooms) 
+            hash = HashCode.Combine(hash, room);
+        
+        return hash;
+    }
+}
---
+++ run.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MazeSort;
+
+var lines = new List<string>();
+string? line;
+
+while ((line = Console.ReadLine()) != string.Empty)
+{
+    lines.Add(line);
+}
+
+var state = GetState(lines);
+
+var result = new Solve().Dijkstra(state);
+Console.WriteLine(result);
+return;
+
+State GetState(List<string> lines)
+{
+    var hallway = lines[1].Replace("#", "");
+        
+    var rooms = new string[4];
+        
+    for(var i = 3; i <= 9; i+=2)
+    {
+        var roomIndex = (i - 3) / 2;
+        var roomChars = new char[lines.Count - 3];
+        for (var j = 2; j < lines.Count - 1; j++)
+        {
+            roomChars[j-2] = lines[j][i];
+        }
+        rooms[roomIndex] = new string(roomChars);
+    }
+    
+    return new State(hallway, rooms);
+}
